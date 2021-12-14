fff4a7d4073bf6c7285e39802749a88b80d3247e
Mahammad Aslam <middeaslam36@gmail.com>
2021-12-14 20:03:59 +0530

    Display the Api Data in Home Page

--- movies-app/src/app/services/movies.service.ts
+++ movies-app/src/app/services/movies.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=125ea7918283e3e7dafdfbf161937dda')
+  }
 }
